6b2b445a16f9e192c15e4c89a4cd387b9aeb0beb
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2020-03-19 11:38:32 +0530

    add session

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+
+app.use(session({
+  secret :'ssshhhhh',
+  resave : false,
+  saveUninitialized : true,
+  }));
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
