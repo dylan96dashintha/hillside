81054b5a93090c5cc5587d3f17c46243b9fce2ea
dylan96dashintha <dashinthadilan.17@cse.mrt.ac.lk>
2020-03-19 13:36:17 +0530

    final

--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -12,8 +12,8 @@ router.get('/', function (req, res, next) {
 router.post('/' ,function(req,res){
     let psw = req.body.inputPsw;
     if(psw == "hill") {
-        req.session.psw = "hill";
-        getAdmin();
+        req.session.flag = true;
+        res.redirect('/admin');
     }else{
         res.redirect('/adminAuth');
     }
@@ -21,49 +21,5 @@ router.post('/' ,function(req,res){
 
 
 
-function getAdmin(){
-    
-    var orderIdList = []  ;
-    var firstNameList = [];
-    var lastNameList = [];
-    var addressList = [];
-    var mobileList = [];
-    var pavementList = [];
-
-    var count_query=`SELECT COUNT(*) as num from customerdet`;
-    conn.query(count_query,function(err,result){
-    
-        if (err) throw error;
-    
-        const count = JSON.parse(JSON.stringify(result));
-    
-    conn.query(`SELECT * FROM customerdet` , function(err , result){
-        if (err) {
-            console.log(err);
-        }else {
-        }
-    
-    
-    const response = JSON.parse(JSON.stringify(result));  
- 
-    for(x=0 ; x<response.length ; x++){
-        orderIdList.push(response[x].orderId);
-        firstNameList.push(response[x].firstname);
-        lastNameList.push(response[x].lastname);
-        addressList.push(response[x].address);
-        mobileList.push(response[x].mobilenum);
-        pavementList.push(response[x].pavement);
-     
-     }
-     //var decoded = jwt.verify(token , 'secret');
-     res.render('admin' , {count:count[0].num,orderId : orderIdList,firstname :firstNameList , lastname :lastNameList , address : addressList , mobilenum : mobileList , pavement :pavementList});
-
-    });
-});
-
-
-
-
-}
 });
 module.exports = router;
