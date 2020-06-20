a444fbe08346dcfd6c336bec4f1c1fa09114d869
RashmikaLakshan <47639124+RashmikaLakshan@users.noreply.github.com>
2020-06-20 21:43:55 +0530

    connection fixed.

--- config/sqlconnection.js
+++ config/sqlconnection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection(
+var connection = mysql.createPool(
     {
         connectionLimit: 10,
         host: 'remotemysql.com',
--- routes/verify.js
+++ routes/verify.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 var nodemailer = require('./nodemailerWithTemp');
-var connection = require('../config/sqlconnection');
+var conn = require('../config/sqlconnection');
 var oid = require('../Modules/createOrderId')
 
 
@@ -13,54 +13,56 @@ router.get('/',(req,res) =>{
 
 router.post('/', (req,res) => {
     var sess = req.session;
+    // console.log(sess)
+    // console.log(req.body)
     if(sess.code == req.body.verify){
         // const hash = bcrypt.hashSync(sess.password, 10);
-
-        connection.connect(function(err) {  
+        conn.getConnection(function(err,connection) {  
             if (err) {
               console.error('error connecting: ' + err.stack);
               return res.render('error'); 
             }
             console.log('connected as id ' + connection.threadId);
-          });
+          
            
-          /* Begin transaction */
-        connection.beginTransaction(function(err) {
-            if (err) { throw err; }
-            // connection.query('INSERT INTO `customerdet` (`firstname`, `lastname`, `address`, `email`, `mobilenum`,`password`) VALUES (?,?,?,?,?,?)',[sess.fname, sess.lname, sess.address, sess.email, sess.mobile,hash], function(err, result) {
-                var orderid = oid();
-            connection.query('INSERT INTO `customerdet` ( `orderId`, `firstname`, `lastname`, `address`, `email`, `mobilenum`) VALUES (?,?,?,?,?,?)',[orderid, sess.fname, sess.lname, sess.address, sess.email, sess.mobile], function(err, result) {
-                if (err) { 
-                    connection.rollback(function() {
-                        res.render('error');
-                        throw err;
-                    });
-                }else{
-                    // var orderid = result.insertId;
-                    console.log('orderid :'+orderid)
-            
-                    connection.query('INSERT INTO `orderdetails` (`checkin`, `checkout`, `orderid`, `roomid`) VALUES (?,?,?,?)',[sess.checkinDate, sess.checkoutDate, orderid, sess.rid], function(err, result) {
-                        if (err) { 
+            /* Begin transaction */
+            connection.beginTransaction(function(err) {
+                if (err) { throw err; }
+                // connection.query('INSERT INTO `customerdet` (`firstname`, `lastname`, `address`, `email`, `mobilenum`,`password`) VALUES (?,?,?,?,?,?)',[sess.fname, sess.lname, sess.address, sess.email, sess.mobile,hash], function(err, result) {
+                    var orderid = oid();
+                connection.query('INSERT INTO `customerdet` ( `orderId`, `firstname`, `lastname`, `address`, `email`, `mobilenum`) VALUES (?,?,?,?,?,?)',[orderid, sess.fname, sess.lname, sess.address, sess.email, sess.mobile], function(err, result) {
+                    if (err) { 
                         connection.rollback(function() {
                             res.render('error');
                             throw err;
                         });
-                        }else{
-                            connection.commit(function(err) {
+                    }else{
+                        // var orderid = result.insertId;
+                        console.log('orderid :'+orderid)
+                
+                        connection.query('INSERT INTO `orderdetails` (`checkin`, `checkout`, `orderid`, `roomid`) VALUES (?,?,?,?)',[sess.checkinDate, sess.checkoutDate, orderid, sess.rid], function(err, result) {
                             if (err) { 
-                                connection.rollback(function() {
+                            connection.rollback(function() {
                                 res.render('error');
                                 throw err;
+                            });
+                            }else{
+                                connection.commit(function(err) {
+                                if (err) { 
+                                    connection.rollback(function() {
+                                    res.render('error');
+                                    throw err;
+                                    });
+                                }
+                                console.log('Transaction Complete.');
+                                // connection.end();
+                                nodemailer.confirmBooking(sess.email, sess.fname, sess.checkIn, sess.checkOut, sess.des);
+                                res.redirect('/index');
                                 });
                             }
-                            console.log('Transaction Complete.');
-                            // connection.end();
-                            nodemailer.confirmBooking(sess.email, sess.fname, sess.checkIn, sess.checkOut, sess.des);
-                            res.redirect('/index');
-                            });
-                        }
-                    });
-                }
+                        });
+                    }
+                });
             });
         });
     }
