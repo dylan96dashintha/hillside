77a701f76aff764cd40112415351a4e85687c797
dylan96dashintha <djdilan09@gmail.com>
2020-07-18 10:56:06 +0530

    arrange the files in perfect order

--- app.js
+++ app.js
@@ -21,10 +21,7 @@ var verifyuser = require('./routes/verify');
 var error = require('./routes/error');
 var logOut = require('./routes/logoutAdmin');
 var test = require('./routes/test');
-var schedule = require('node-schedule');
-var conn = require('./config/sqlconnection');
-var addsubtractDate = require('add-subtract-date');
-var nodemailer = require('./routes/nodemailerWithTemp');
+var reminder = require('./Modules/emailReminder');
 var app = express();
 
 require("firebase/firestore");
@@ -56,38 +53,8 @@ app.use(session(
   }
 ))
 
-//schedule for send remainder email
-var j = schedule.scheduleJob('01 00 * * *', function(){
-  console.log("hi");
-  conn.query(`SELECT * FROM customerdetails NATURAL JOIN orderdetails ORDER BY bookDate ASC` , function(err , result){
-    if (err) {
-        console.log(err);
-    }else {
-    }
-  const response = JSON.parse(JSON.stringify(result)); 
-  for(x = 0; x<response.length; x++) {
-    const checkIn =new Date((response[x].checkIn).substring(0,10));
-    const bookDate = new Date((response[x].bookDate).substring(0,10));
-    var diffDays = parseInt((checkIn - bookDate) / (1000 * 60 * 60 * 24));
-    if (diffDays > 2 ) {
-      var pavementDate = addsubtractDate.subtract(new Date(checkIn), 2, "days");
-      var datetime = new Date();
-      var today = new Date(datetime.toISOString().slice(0,10));
-      var difBetweenPavementCurrent = parseInt((pavementDate - today) / (1000 * 60 * 60 * 24)); 
-      if(difBetweenPavementCurrent === 1 || difBetweenPavementCurrent === 0  ) {
-        const email = response[x].email;
-        const name = response[x].fname;
-        const orderId = response[x].orderId;
-        nodemailer.reminder(email, name, orderId, pavementDate.toISOString().slice(0,10));
-      }
-       
-    }
-  }
-  });
-});
-
-
-
+//send reminder mail
+reminder();
 
 
 app.use(express.urlencoded({ extended: false }));
